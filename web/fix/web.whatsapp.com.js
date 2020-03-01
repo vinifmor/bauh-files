0dce5462d1f241281d659133c541db7240c4d8d0
Vinícius Moreira <vinicius_fmoreira@hotmail.com>
2020-03-01 19:12:04 -0300

    [web][fix][web.whatsapp.com]

--- web/fix/web.whatsapp.com.js
+++ web/fix/web.whatsapp.com.js
@@ -1,3 +1,2 @@
-// Source: https://github.com/DocBox12/WhatsApp-nativefier-fix
-if (document.body.innerText.replace(/\n/g, ' ').search(/whatsapp works with.*to use whatsapp.*update/i) !== -1)
+if (document.body.innerText.replace(/\n/g, ' ').search(/Chrome/i) !== -1)
 	navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
