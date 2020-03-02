8eb87ae8ede747ce7f44461a2ed8f18742ad105f
Vinícius Moreira <vinicius_fmoreira@hotmail.com>
2020-03-02 11:26:01 -0300

    [web][fix][web.whatsapp.com]

--- web/fix/web.whatsapp.com.js
+++ web/fix/web.whatsapp.com.js
@@ -1,2 +1,2 @@
-if (document.body.innerText.replace(/\n/g, ' ').search(/Chrome/i) !== -1)
+if (document.body.innerText.search(/Chrome/i) !== -1)
 	navigator.serviceWorker.getRegistration().then(function (r) { r.unregister(); document.location.reload() });
